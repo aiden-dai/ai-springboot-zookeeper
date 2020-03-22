dd68765cc3a0b5010d7c0926107a8c6a3781a952
aiden-dai  <aiden.dai@gmail.com>
2020-03-22 10:55:40 +0800

    more

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ai-springboot-zookeeper
 Test springboot with zookeeper
+
+
+
+docker run --rm -d --name zookeeper \
+-p 2181:2181 \
+zookeeper
---
+++ discovery-server/src/main/java/com/example/demo/controller/ServerController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.zookeeper.serviceregistry.ServiceInstanceRegistration;
+import org.springframework.cloud.zookeeper.serviceregistry.ZookeeperRegistration;
+import org.springframework.cloud.zookeeper.serviceregistry.ZookeeperServiceRegistry;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerController{
+
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+
+
+    // @Autowired
+    // private ZookeeperServiceRegistry serviceRegistry;
+
+    // public void registerThings() {
+    //     ZookeeperRegistration registration = ServiceInstanceRegistration.builder()
+    //             .defaultUriSpec()
+    //             .address("anyUrl")
+    //             .port(10)
+    //             .name("/a/b/c/d/anotherservice")
+    //             .build();
+    //     this.serviceRegistry.register(registration);
+    // }
+
+    
+
+    
+}
