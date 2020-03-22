c2e2683f5447b02f53df0fa14ad96d738bcc7b25
aiden-dai  <aiden.dai@gmail.com>
2020-03-22 11:35:30 +0800

    more

--- discovery-client/src/main/java/com/zk/client/controller/ClientController.java
+++ discovery-client/src/main/java/com/zk/client/controller/ClientController.java
@@ -1,14 +1,18 @@
-package com.example.demo.controller;
+package com.zk.client.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.zookeeper.serviceregistry.ServiceInstanceRegistration;
 import org.springframework.cloud.zookeeper.serviceregistry.ZookeeperRegistration;
 import org.springframework.cloud.zookeeper.serviceregistry.ZookeeperServiceRegistry;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class ServerController{
+public class ClientController{
 
 
     @RequestMapping("/")
@@ -30,7 +34,27 @@ public class ServerController{
     //     this.serviceRegistry.register(registration);
     // }
 
-    
 
+    @LoadBalanced
+	@Bean
+	public RestTemplate loadbalancedRestTemplate() {
+		return new RestTemplate();
+	}
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+
+		// model.addAttribute("task", new Task());
+
+		String uri = "http://zk-server/";
+		
+		String response = restTemplate.getForObject(uri, String.class);
+
+		return response;
+	}
     
 }
--- discovery-server/src/main/java/com/zk/server/controller/ServerController.java
+++ discovery-server/src/main/java/com/zk/server/controller/ServerController.java
@@ -1,18 +1,14 @@
-package com.example.demo.controller;
+package com.zk.server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.zookeeper.serviceregistry.ServiceInstanceRegistration;
 import org.springframework.cloud.zookeeper.serviceregistry.ZookeeperRegistration;
 import org.springframework.cloud.zookeeper.serviceregistry.ZookeeperServiceRegistry;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class ClientController{
+public class ServerController{
 
 
     @RequestMapping("/")
@@ -34,27 +30,7 @@ public class ClientController{
     //     this.serviceRegistry.register(registration);
     // }
 
+    
 
-    @LoadBalanced
-	@Bean
-	public RestTemplate loadbalancedRestTemplate() {
-		return new RestTemplate();
-	}
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index() {
-
-		// model.addAttribute("task", new Task());
-
-		String uri = "http://zk-server/";
-		
-		String response = restTemplate.getForObject(uri, String.class);
-
-		return response;
-	}
     
 }
